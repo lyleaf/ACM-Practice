571925f220e43dd94454c3c21f40fffa9aa6b9e5
Liu Yiling <lyleaf@gmail.com>
2016-05-30 11:09:59 +0200

    Update Leetcode - Counting Bits.cpp

--- Leetcode - Counting Bits.cpp
+++ Leetcode - Counting Bits.cpp
@@ -1,3 +1,16 @@
+/*给出一个数num，比如说5，return 从0到5这6个数里每一个数的二进制表示里含有1个个数.
+
+我的方法是直接用mod和移位。可是取模让这个算法有一点expensive.
+看到的别人的方法有一种非常神奇。他先把这个vector全部初始化为0.
+然后的思路是：Divide the numbers in ranges like [2-3], [4-7], [8-15] and so on. And try to generate new range from previous.
+
+SKill Set: 
+If I want to toggle between 0 and 1, I can use
+int flag = 1;
+flag = !flag;
+
+
+*/
 class Solution {
 public:
     vector<int> countBits(int num) {
